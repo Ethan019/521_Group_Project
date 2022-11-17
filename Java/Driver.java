7cf7f4e068c955c010c165d2c935f61ec50983ea
Ethan019 <70040164+Ethan019@users.noreply.github.com>
2022-11-17 13:30:50 -0500

    Update Driver.java

--- Java/Driver.java
+++ Java/Driver.java
@@ -1,9 +1,10 @@
 
+
 //Ethan Massingill
 import java.io.*;
+import java.util.Objects;
 import java.util.Scanner;
 import java.sql.*;
-
 
 public class Driver {
 
@@ -14,67 +15,56 @@ public class Driver {
     	Connection conn;
 		try
 		{
-			String url = "jdbc:mysql://localhost/bankaccount";  //test is the database name
-			String user = "Student"; //username THIS WILL CHANGE TO AN OBJECT PASSED IN BY LOGIN. DEPENING ON ROLL THIS WILL ONLY BE SELECTED IF CURRENT USER IS SELECTED
-			String password = "t123456"; //root password, you set it when you install the DBMS
-
-			PreparedStatement st = conn.PreparedStatement("select* from users where uname=? and pass=?");
-			st.setString(1, user);
-			st.setString(2, password);
-			ResultSet rs = st.executeQuery();
-
-			if(rs.next()){
-				String username = rs.getString("uname");
-				String role = rs.getString("role");
-				if (role=null || role.isEmpty()){
-					return;
-				}
-				else if (role.equals("student")){
-					
-				}
-			}
-			
-		    //print menu
-
-
-			//temp values for error checking DELETE 
-			String STUDID = "U1234";
-			String CRN = "2345";
-			String COURSENUM = "U101";
-			String DEPART = "SCI";
-
-
-			//END OF TEMP VALUES FOR TEST CASE
-
-			Scanner scan = new Scanner(System.in);
-			//CREATES THE SQL CUSTOM CLASS FROM STUDENTSSQL;
-			StudentSQL SQL = new StudentSQL();
-			int command;
 			String input;
-	        boolean keepGoing = true;
-	        while (keepGoing == true)
-	        {
-	            printmenu();
-	            input = scan.nextLine();
-	            command = Integer.parseInt(input);
-				
-				switch(command)
-				{
-					case 1: SQL.studentView(conn, STUDID);; break;
-					case 2: SQL.add_course(conn, STUDID, CRN, COURSENUM); break;
-					case 3: SQL.student_drop(conn, CRN, STUDID); break;
-					case 4: SQL.add_student(conn, scan); break;	
-					case 5: SQL.delete_student(conn, scan); break;
-					case 6: SQL.register_course(conn, scan); break;
-					case 7: SQL.show_regist(conn, scan); break;
-					
-					case 0: 
-						System.out.println("The Session has been ended, Thank you!");
-	               	    keepGoing = false;
-	               	    conn.close(); //close database connection   
-						break;
-				}    		            			
-		    }
+			System.out.print("Please input ID: ");
+			Scanner scan = new Scanner(System.in);
+			String userID = scan.nextLine();
+			System.out.print("please input Password: ");
+			String Newpassword = scan.nextLine();
+			String url = "jdbc:mysql://localhost/sys";  //test is the database name
+			String user = "root"; //username
+			String password = "Password12345"; //root password, you set it when you install the DBMS
+
+			conn = DriverManager.getConnection(url, user, password);
+		    //print menu
+			
+
+			Statement st = conn.createStatement();
+			
+			String query = "select* from users where USERID= '"+userID +"' AND Password= '"+Newpassword+"';";
+
+			ResultSet rs = st.executeQuery(query);
+			String role = null;
+			while(rs.next()){
+				role = rs.getString("role");
+			}
+
+
+			
+			//only for student
+
+			if(Objects.equals(role, "Student")) {
+				System.out.println("trying to connect...");
+				conn = DriverManager.getConnection("jdbc:mysql://localhost/sys", "Student", "StudentPassword");
+				Student(conn, userID);
+			}
+			else if(role=="Professor"){
+				conn = DriverManager.getConnection("jdbc:mysql://localhost/sys", "Professor", "ProfessorPassword");
+				Professor(conn, userID);
+			} else if (role=="Schooladmin") {
+				conn = DriverManager.getConnection("jdbc:mysql://localhost/sys", "Schooladmin", "SchoolPassword");
+				SchoolAdmin(conn, userID);
+			} else if (role=="Dbadmin") {
+				conn = DriverManager.getConnection("jdbc:mysql://localhost/sys", "Dbadmin", "DatabadminPassword");
+				DatabaseAdmin(conn, userID);
+			} else if (role==null) {
+				conn.close();
+				rs.close();
+				return;
+			}
+
+
+
 		}
 		catch (SQLException ex)
 		{
@@ -97,14 +87,69 @@ public class Driver {
 	      	System.out.println("1. Add a course");
 	      	System.out.println("2. Show all course");
 			System.out.println("3. Delete a course");
-			System.out.println("4. Add a Student");
-			System.out.println("5. Delete a Student"); 
-			System.out.println("6. Register a course");
+			//System.out.println("4. Add a Student");
+			//System.out.println("5. Delete a Student");
+			//System.out.println("6. Register a course");
 			System.out.println("7. Check student registration");   
 	      	System.out.println("0. Quit ");
 	      	System.out.println();
 	      	System.out.println("Please choose an option");
     }
+
+
+
+
+	public static void Student(Connection conn, String studid) throws SQLException, IOException {
+		int command;
+
+		boolean keepGoing = true;
+		while (keepGoing == true) {
+			printmenu();
+			String input;
+
+			//TESTING HARD CODE
+			String crn1 = "12345";
+
+			Scanner scan = new Scanner(System.in);
+			input = scan.nextLine();
+			command = Integer.parseInt(input);
+			StudentSQL SQL = new StudentSQL();
+			switch (command) {
+				case 1:
+					SQL.add_course(conn, studid, crn1);
+					System.out.println("course was sucessfully registered!");
+					break;
+				case 2:
+					//SQL.show_courses(conn);
+					break;
+				case 3:
+					//SQL.delete_course(conn, scan);
+					break;
+				//case 4: SQL.add_student(conn, scan); break;
+				//case 5: SQL.delete_student(conn, scan); break;
+				//case 6: SQL.register_course(conn, scan); break;
+				case 7:
+					//SQL.show_regist(conn, scan);
+					break;
+
+				case 0:
+					System.out.println("The Session has been ended, Thank you!");
+					keepGoing = false;
+					conn.close(); //close database connection
+					break;
+			}
+		}
+	}
+	public static void Professor(Connection conn, String profid){
+		
+	}
+	public static void SchoolAdmin(Connection conn, String SCHADID){
+		
+	}
+	public static void DatabaseAdmin(Connection conn, String DBAID){
+		
+	}
 }
+
 
 
