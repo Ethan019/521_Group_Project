a888f70d44818f72c7e142669cfe54da1033053c
elizandrews <61242894+elizandrews@users.noreply.github.com>
2022-12-04 04:39:56 -0500

    Update DBASQL.java

--- Java/DBASQL.java
+++ Java/DBASQL.java
@@ -25,60 +25,62 @@ public class DBASQL {
     }
 
     // ********************** COURSE FUNCTIONS *******************
-    // Add a course
-    public void AddCourse(Connection conn, String DEPT_KEY, String COURSE_NUM, String DESC, String PROF_NAME) throws SQLException, IOException {
+    // Add a course - Original Caroline, New Ethan & Liz
+    public  boolean AddCourse(Connection conn,  String COURSE_NUM, String COURSE_NAME, String DES, String PROF_NAME, String Semester, String Year, String Section_NUM ){
         try {
             Statement st = conn.createStatement();
-            Random rand = new Random();
 
-            int CRN = rand.nextInt(10000, 49999);
-            String add_course_query = "INSERT INTO COURSE(DEPTKEY, COURSENUM, PROFNAME, DESC, CRN) VALUES ('" + DEPT_KEY + "', '" + COURSE_NUM + "', '" + PROF_NAME + "', '" + DESC + "', '" + CRN + "');";
-            st.executeQuery(add_course_query);
 
-            String select_query = "SELECT * FROM COURSE WHERE CRN = '" + CRN + "'';";
-            ResultSet rs = st.executeQuery(select_query);
+            String adept ="Select DEPTKEY, PROFID FROM PROFESSOR WHERE PROFNAME = '"+PROF_NAME+"';";
+            ResultSet r2 = st.executeQuery(adept);
 
-            if(rs.next() == true) {
-                System.out.println("Course is in the system.");
-                return;
+            if(r2.next()) {
+                String deptk = r2.getString("DEPTKEY");
+                String profi = r2.getString("PROFID");
+                System.out.println(deptk);
+                String add_course_query = "INSERT INTO COURSE(DEPTKEY, coursename, COURSENUM, SECTIONNUM, PROFID, PROFNAME, DES, SEMESTER, YEAR) VALUES('" + deptk + "', '" + COURSE_NAME + "', '" + COURSE_NUM + "', '" + Section_NUM + "', " + profi + ", '"+PROF_NAME+"', '"+DES+"', '"+Semester+"', '"+Year+"');";
+                st.executeUpdate(add_course_query);
+                return true;
             }
-            else {
-                System.out.println("Course not added.");
+
+
+            //String select_query = "SELECT * FROM COURSE WHERE COURSENUM = '" + COURSE_NUM + "';";
+            //ResultSet rs = st.executeQuery(select_query);
+
+            //if(rs.next() == true) {
+              //  System.out.println("This course is in the system.");
+               // return true;
+            //}
+
+
+        }
+        catch(SQLException e) {
+            System.out.println("Error: " + e.getMessage() +"Please try to add course again");
+            return false;
+        }
+        return  false;
+    }
+
+    //Delete a course - Caroline & Ethan Original, Liz Modified
+    public boolean DeleteCourse(Connection conn, String CRN, String ID) {
+        try {
+            Statement st = conn.createStatement();
+            
+            String deleter = "DELETE FROM REGISTEREDFOR WHERE CRN = "+CRN+";";
+            st.executeUpdate(deleter);
+            String delete_query = "DELETE FROM COURSE WHERE CRN ="+CRN+";";
+            st.executeUpdate(delete_query);
+            System.out.println("Course was deleted");
+            return true;
+                    
             }
         }
         catch(SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        st.close();
-        rs.close();
-        sc.close();
+        return false;
     }
 
-    //Delete a course
-    public void DeleteCourse(Connection conn, String COURSE_NUM, String DEPT_KEY) throws SQLException, IOException {
-        try {
-            Statement st = conn.createStatement();
-
-            String delete_query = "DELETE * FROM COURSE WHERE COURSENUM = '" + DEL_COURSE_NUM + "' AND DEPTKEY = '" + DEPT_KEY + "';";
-            st.executeQuery(delete_query);
-
-            String select_query = "SELECT * FROM COURSE WHERE COURSENUM = '" + DEL_COURSE_NUM + "' AND DEPTKEY = '" + DEPT_KEY + "';";
-            ResultSet rs = st.executeQuery(select_query);
-            if(rs.next() == false) {
-                System.out.println("Course deleted.");
-                return;
-            }
-            else {
-                System.out.println("Course not deleted. Please try again.");
-            }
-        }
-        catch(SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-        st.close();
-        rs.close();
-        sc.close();
-    }
 
     //Modify a course 
     public void UpdateCourse(Connection conn, String CRN, String DESC, String DEPT_KEY, String COURSE_NUM, String SECT_NUM, String PROF_ID, String PROF_NAME, String SEMESTER, String YEAR) throws SQLException, IOException {
@@ -140,54 +142,71 @@ public class DBASQL {
         return false;
     }
      
-    //Delete a student
-    public void DelStud(Connection conn, Scanner scan, String STUD_ID) throws SQLException, IOException {
+    //Delete a student - Caroline Original, Liz & Ethan New
+    public boolean DeleteStudent(Connection conn, String Fname, String Lname, String Major){
         try {
             Statement st = conn.createStatement();
-            
-            String del_student = "DELETE FROM STUDENT WHERE STUDID = '" + STUD_ID + "';";
-            st.executeUpdate(del_student);
+            String stud = "SELECT STUDID FROM STUDENT WHERE FNAME = '"+Fname+"' AND LNAME='"+Lname+"' AND MAJOR='"+Major+"';";
+            ResultSet rs1 = st.executeQuery(stud);
+            while(rs1.next()){
+                String studid = rs1.getString("STUDID");
+                String query = "select * from registeredfor where studid ="+studid+";";
+                ResultSet rs2=st.executeQuery(query);
+                if(rs2.next()){
+                    String dele_course ="DELETE FROM REGISTEREDFOR WHERE STUDID ="+studid+";";
+                    st.executeUpdate(dele_course);
+                    String del_student = "DELETE FROM STUDENT WHERE STUDID ="+studid+";";
+                    st.executeUpdate(del_student);
+                    String dele_stuser = "DELETE FROM USERS WHERE STUDID = "+studid+";";
+                    st.executeUpdate(dele_pruser);
+                    return true;
 
-            String check = "SELECT * FROM STUDENT WHERE STUDID = '" + STUD_ID + "';";
-            ResultSet rs = st.executeQuery(check);
-            if(rs.next() == false) {
-                System.out.println("Student deleted.");
-                return;
+                }else{
+                    String del_student = "DELETE FROM STUDENT WHERE STUDID ="+studid+";";
+                    st.executeUpdate(del_student);
+                    String dele_stuser = "DELETE FROM USERS WHERE STUDID = "+studid+";";
+                    st.executeUpdate(dele_pruser);
+                    return true;
+                }
+
+
+
             }
+
+
         }
         catch(SQLException e) {
             System.out.println("Error: " + e.getMessage());
+            return false;
         }
-        st.close();
-        rs.close();
-        sc.close();
+        return false;
     }
 
-    //Update student info
-    public void UpdateStud(Connection conn, Scanner scan, String STUD_ID, String F_NAME, String L_NAME, String MAJOR, String CLASS) throws SQLException, IOException {
+    //Update student info - Caroline original, Liz & Ethan Update
+    public void EditStud(Connection conn, Scanner scan, String STUD_ID, String F_NAME, String L_NAME, String MAJOR) throws SQLException, IOException {
         try {
             Statement st = conn.createStatement();
 
-            String update_query = "UPDATE STUDENT SET FNAME = '" + F_NAME + "', LNAME = '" + L_NAME + "', MAJOR = '" + MAJOR + ", CLASS = '" + CLASS + "' WHERE STUDID = '" + STUD_ID + "';";
+            String update_query = "UPDATE STUDENT SET FNAME = '" + F_NAME + "', LNAME = '" + L_NAME + "', MAJOR = '" + MAJOR + "' WHERE STUDID = '" + STUD_ID + "';";
             st.executeUpdate(update_query);
 
-            String select_query = "SELECT * FROM STUDENT WHERE STUDID = '" + stud + "';";
+            String select_query = "SELECT * FROM STUDENT WHERE STUDID = '" + STUD_ID + "';";
             ResultSet rs = st.executeQuery(select_query);
-            
+
             if(rs.next() == true) {
-                System.out.println("Update successful.");
+                System.out.println("The input information is already current.");
                 return;
             }
+
             else {
-                System.out.println("Try again.");
+                System.out.println("Information updated successfully.");
             }
         }
         catch(SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        rs.close();
-        st.close();
-        }
+
+    }
 
     // This will get and display information when "modify student registration" is selected
     public void RegInfo(Connection conn, Scanner scan, String STUD_ID) throws SQLException, IOException {
@@ -204,6 +223,23 @@ public class DBASQL {
         System.out.println("Grade: ");
 
         st.close();
+    }
+
+    //Add Student to Course - 
+    public void AddStudToCourse(Connection conn, String STUD_ID, String CRN) {
+        try {
+            Statement st = conn.createStatement();
+
+            // Display registered courses first
+            String select_query = "SELECT * FROM REGISTEREDFOR WHERE STUDID = '" + STUD_ID + " AND CRN = "+CRN+";";
+            ResultSet rs1 = st.executeQuery(select_query);
+            if(!rs1.next()) {
+                String insert_student="Insert into REGISTEREDFOR(STUDID, CRN) VALUES("+STUD_ID+","+CRN+");";
+                st.executeUpdate(insert_student);
+            }
+        }catch (SQLException e ) {
+            System.out.println(e.getMessage());
+        }
     }
 
     // Update grade
@@ -272,53 +308,87 @@ public class DBASQL {
         return false;
     }
 
-    //Delete professor
-    public void DelProfessor(Connection conn, Scanner scan, String PROF_ID) {
+    //Delete professor - Original Caroline, Updated version Liz & Ethan
+    public boolean DeleteProfessor(Connection conn, String Fname, String Lname, String Depart){
         try {
             Statement st = conn.createStatement();
-            // Enter ID of professor to remove
+            String stud = "SELECT PROFID FROM PROFESSOR WHERE FNAME = '"+Fname+"' AND LNAME='"+Lname+"' AND DEPTKEY='"+Depart+"';";
+            ResultSet rs1 = st.executeQuery(stud);
+            while(rs1.next()){
+                String profid = rs1.getString("PROFID");
+                String query = "select * from course where PROFID ="+profid+";";
+                ResultSet rs2=st.executeQuery(query);
+                if(rs2.next()){
+                    String crn = rs2.getString("CRN");
+                    ResultSet rs3=st.executeQuery("Select CRN FROM REGISTEREDFOR WHERE CRN ="+crn+";");
 
-            String del_prof = "DELETE FROM PROFESSOR WHERE PROFID = '" + PROF_ID + "';";
-            st.executeUpdate(del_prof);
+                    if(rs3.next()){
+                        String dele_reg = "Delete FROM REGISTEREDFOR WHERE CRN ="+crn+";";
+                        st.executeUpdate(dele_reg);
+                        String dele_course ="DELETE FROM COURSE WHERE PROFID ="+profid+";";
+                        st.executeUpdate(dele_course);
+                        String del_prof = "DELETE FROM Professor WHERE STUDID ="+profid+";";
+                        st.executeUpdate(del_prof);
+                        String dele_pruser = "DELETE FROM USERS WHERE PROFID = "+profid+";";
+                        st.executeUpdate(dele_pruser);
+                        return true;
+                    }else{
+                        String dele_course ="DELETE FROM COURSE WHERE PROFID ="+profid+";";
+                        st.executeUpdate(dele_course);
+                        String del_student = "DELETE FROM PROFESSOR WHERE PROFID ="+profid+";";
+                        st.executeUpdate(del_student);
+                        String dele_pruser = "DELETE FROM USERS WHERE PROFID = "+profid+";";
+                        st.executeUpdate(dele_pruser);
+                        return true;
+                    }
 
-            String check = "SELECT * FROM PROFESSOR WHERE PROFID = '" + PROF_ID + "';";
-            ResultSet rs = st.executeQuery(check);
-            if(rs.next() == false) {
-                System.out.println("Professor deleted.");
+
+                }else{
+                    String del_prof3 = "DELETE FROM PROFESSOR WHERE PROFID ="+profid+";";
+                    st.executeUpdate(del_prof3);
+                    String dele_pruser = "DELETE FROM USERS WHERE PROFID = "+profid+";";
+                    st.executeUpdate(dele_pruser);
+                    return true;
+                }
+
+
+
+            }
+
+
+        }
+        catch(SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+            return false;
+        }
+        return false;
+    }
+
+//Edit Professor Information - Original Caroline, Update Liz & Ethan
+    public void EditProf(Connection conn, Scanner scan, String PROF_ID, String F_NAME, String L_NAME, String DEPARTMENT) throws SQLException, IOException {
+        try {
+            Statement st = conn.createStatement();
+
+            String update_query = "UPDATE PROFESSOR SET FNAME = '" + F_NAME + "', LNAME = '" + F_NAME + "', DEPARTMENT = '" + DEPARTMENT + "' WHERE PROFID = '" + PROF_ID + "';";
+            st.executeUpdate(update_query);
+
+            String select_query = "SELECT * FROM PROFESSOR WHERE PROF_ID = '" + PROF_ID + "';";
+            ResultSet rs = st.executeQuery(select_query);
+            if(rs.next() == true) {
+                System.out.println("Professor added.");
                 return;
+            }
+            else {
+                System.out.println("Try again.");
             }
         }
         catch(SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        st.close();
-        rs.close();
+
     }
 
-    public void UpdateProf(Connection conn, Scanner scan, String PROF_ID, String F_NAME, String L_NAME, String DEPT_KEY) throws SQLException, IOException {
-        try {
-            Statement st = conn.createStatement();
-        
-            String update_query = "UPDATE PROFESSOR SET FNAME = '" + F_NAME + "', LNAME = '" + L_NAME + "', DEPARTMENT = '" + DEPT_KEY + "' WHERE PROFID = '" + PROF_ID + "';";
-            st.executeUpdate(update_query);
-        
-            String select_query = "SELECT * FROM STUDENT WHERE STUDID = '" + STUD_ID + "';";
-            ResultSet rs = st.executeQuery(select_query);
-
-            if(rs.next() == true) {
-                System.out.println("Update successful.");
-                return;
-                }       
-                else {
-                    System.out.println("Try again.");
-                }
-            }
-            catch(SQLException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-            rs.close();
-            st.close();                
-        }
+}
             
     public void RegStud(Connection conn, Scanner scan, String STUD_ID, String CRN, String COURSE_NUM, String GRADE) throws SQLException, IOException {
         try {
@@ -484,18 +554,22 @@ public class DBASQL {
         return false;
     }
 
-    // Delete administrator
+    // Delete administrator - Updated 12/3 by Liz
     public void DelAdmin(Connection conn, String ADMIN_ID) throws SQLException, IOException {
         try {
             Statement st = conn.createStatement();
 
             String delete_query = "DELETE * FROM SCHADMIN WHERE SCHADID = '" + ADMIN_ID + "';";
             st.executeUpdate(delete_query);
+            String delete_uquery = "DELETE * FROM USERS WHERE SCHADID = '" + ADMIN_ID + "';";
+            st.executeUpdate(delete_uquery);
 
             String select_query = "SELECT * FROM SCHADMIN WHERE SCHADID = '" + ADMIN_ID + "';";
             ResultSet rs = st.executeQuery(select_query);
+            String select_uquery = "SELECT * FROM USERS WHERE SCHADID = '" + ADMIN_ID + "';";
+            ResultSet rs1 = st.executeQuery(select_uquery);
 
-            if(rs.next == false) {
+            if(rs.next == false AND rs1.next == false) {
                 System.out.println("Administrator deleted.");
             }
             else {
